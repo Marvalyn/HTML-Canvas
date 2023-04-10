ef5583316fa083fb8f8537f051b11e3416acfb26
Marvalyn <111514523+Marvalyn@users.noreply.github.com>
2023-04-10 19:57:02 +0100

    stroke settings

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-html, body {
-    margin: 0;
-  }
+const canvas = document.querySelector("#draw");
+
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+//setting the stroke to a colour and setting it so that when lines join and cap the line will be rounded at that point
+
+ctx.strokeStyle = "BADA55";
+ctx.lineJoin = "round";
+ctx.lineCap = "round";
